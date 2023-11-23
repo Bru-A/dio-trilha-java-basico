1d968e4024e6ba5dc9bf670571a0ae2435b9af38
Bruna <brualves.code@gmail.com>
2023-11-23 19:33:37 +0000

    Working

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor digite o número da Agência");
+        numero = scanner.nextInt();
+        agencia = scanner.nextLine();
+        System.out.println("Por favor digite a agência!");
+        agencia = scanner.nextLine();
+        System.out.println("Por favor digite o nome do cliente");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Por favor digite o saldo da conta");
+        saldo= scanner.nextDouble();
+
+        
+
+        System.out.println("Olá, " + nomeCliente + "! Obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque.");
+        scanner.close();
+
     }
 }
